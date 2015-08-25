82bc47dd110381ac368e1c88169a8c401748d347
souparno <souparno.majumder@gmail.com>
2015-08-25 19:20:13 +0530

    filename typo

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-requirejs.config({
+/*requirejs.config({
   "baseUrl": "/",
   "paths": {
     "calculator": "js/calculator.js"
   }
 });
-
+*/
 
 define(function (require, exports) {
   var calculator = require('calculator');
-  var result = calulator.add(1, 2);
+  var result = calculator.add(1, 2);
 
   console.log(result);
 });
